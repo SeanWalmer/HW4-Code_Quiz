c0282036a47811359d84adf2a7eaf512c4f3afeb
SeanW <sean.walmer@outlook.com>
2020-04-04 22:28:00 -0400

    just lost my long message so heres the sort of it. reports now working to store leaderboar scores into local storage! just need to create high scores page and send player there when they submit and all done.

--- reportScript.js
+++ reportScript.js
@@ -1,6 +1,31 @@
+var initials = ""
+
 // grabs local storage and element and then sets element equal to storage
 var score = document.querySelector("#score");
 var result = localStorage.getItem("result");
 score.textContent = result;
 
+// creating the player object to store High scores
+var leaderboard = JSON.parse(localStorage.getItem("leaderboard"))
+if( leaderboard === null){
+leaderboard = {
+    initial: [],
+    score: []
+};
+};
+console.log(leaderboard)
 // store the player high score to local storage
+var submit = document.querySelector("#submit");
+
+submit.addEventListener("click", function(){
+    event.preventDefault();
+    initials = document.querySelector("#hsInitial").value;
+    
+    if(initials === ""){
+        alert("You must fill out the form to submit your score")
+    }else{
+        leaderboard.initial.push(initials)
+        leaderboard.score.push(result)
+        localStorage.setItem("leaderboard", JSON.stringify(leaderboard))
+    }
+});
